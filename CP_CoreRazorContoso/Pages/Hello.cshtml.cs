4172f479e99af34ba90db0c4007dcfc3112976aa
Christian Partington <21810292@bucks.ac.uk>
2019-09-30 11:56:38 +0100

    Add project files.

---
+++ CP_CoreRazorContoso/Pages/Hello.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CP_CoreRazorContoso.Pages
+{
+    public class HelloModel : PageModel
+    {
+        public string Name { get; set; }
+        public void OnGet()
+        {
+            Name = null;
+        }
+
+        public void OnPost()
+        {
+            this.Name = Request.Form ["Name"];
+        }
+    }
+}
